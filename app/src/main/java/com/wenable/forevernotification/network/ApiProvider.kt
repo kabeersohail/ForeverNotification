8f7e24b11b0ff9cd1786f2844c2b26dce850ec31
kabeersohail <kabeersohel@gmail.com>
2023-09-20 20:14:58 +0530

    added api provider

---
+++ app/src/main/java/com/wenable/forevernotification/network/ApiProvider.kt
@@ -0,0 +1,15 @@
+package com.wenable.forevernotification.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiProvider {
+    private const val BASE_URL = "https://demo6055087.mockable.io/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
+}
